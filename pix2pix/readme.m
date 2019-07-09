0c4889a67a8893259aa29c1fa4b20ffa45dace6c
Md Selim <10825767+selim-iitdu@users.noreply.github.com>
2019-07-09 11:13:34 -0400

    Update readme.m

--- pix2pix/readme.m
+++ pix2pix/readme.m
@@ -1,1 +1,5 @@
 Pix2pix is a cGAN which is use to generate sythesize image. This is used to distinguish the dataset as easy and hard also this is considered as a benchmark to evaluate our proposed GAN model.
+
+#trained model
+
+The trained model is stored in onedrive and also in MSU server
